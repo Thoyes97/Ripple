7336b6b278424a40a0944aeb0fadb447f56da5db
Tryston Hoyes <trystonhoyes@gmail.com>
2019-04-04 11:30:35 -0400

    Initial Commit

---
+++ Ripple/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ripple
+//
+//  Created by Tryston Hoyes on 2019-04-04.
+//  Copyright © 2019 Tryston Hoyes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
